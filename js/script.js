403cb66ec888b3d6104235e989451647693375df
ukhty anjani <ukhtyanjani8@gmail.com>
2025-06-22 23:43:40 +0700

    Form Pencarian

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Toggle class active
+// Toggle class active untuk hambuger menu
 const navbarnav = document.querySelector(".navbar-nav");
 // Ketika hamburger menu di klik
 document.querySelector("#hamburger-menu").onclick = () => {
@@ -7,8 +7,25 @@ document.querySelector("#hamburger-menu").onclick = () => {
 
 // Klik di luar sidebar untuk menghilang nav
 const hamburger = document.querySelector("#hamburger-menu");
+// toggle class active untuk search form
+const searchform = document.querySelector(".search-form");
+const searchbox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchform.classList.toggle("active");
+  searchbox.focus();
+  e.preventDefault();
+};
+
+// Klik di luar elemen
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
+
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarnav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarnav.contains(e.target)) {
     navbarnav.classList.remove("active");
+  }
+  if (!sb.contains(e.target) && !searchform.contains(e.target)) {
+    searchform.classList.remove("active");
   }
 });
