7be5ed6f89d222d653edf2fc0438972b4179b39d
ukhty anjani <ukhtyanjani8@gmail.com>
2025-06-25 15:09:43 +0700

    menambahkan icon star dan tampilan harga

--- src/app.js
+++ src/app.js
@@ -9,3 +9,12 @@ document.addEventListener("alpine:init", () => {
     ],
   }));
 });
+
+// konversi ke Rupiah
+const rupiah = (number) => {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    minimumFractionDigits: 0,
+  }).format(number);
+};
